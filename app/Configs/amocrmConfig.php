f1d7bd67bd6ee38a00f3952351a32227f959db51
EvgeniyLutsenko <evgnelucen@gmail.com>
2022-11-10 15:18:40 +0300

    install amo, bizon, update amo config

--- app/Configs/amocrmConfig.php
+++ app/Configs/amocrmConfig.php
@@ -8,57 +8,60 @@ class amocrmConfig
     public const DIR_ENV = __DIR__ . DIRECTORY_SEPARATOR . '../../.env';
     public const DIR_TOKEN_FILE = __DIR__ . DIRECTORY_SEPARATOR . '../amo_token_file.json';
     /* LEADS CF */
-    public const SB_CF_ID = 379401;
+    public const SB_CF_ID = 976075;
     public const DSM_CF_IF = '';
-    public const CF_ID_PRODUCT_NAME = 379105;
+    public const CF_ID_PRODUCT_NAME = 976177;
 
     /* ANALYTICS MARKUP */
-    public const FBP_CF_ID = 378997;
-    public const GOOGLE_CID_CF_ID = 282449;
-    public const UTM_SOURCE_CF_ID = 282429;
-    public const UTM_MEDIUM_CF_ID = 282425;
-    public const UTM_CAMPAIGN_CF_ID = 282427;
-    public const UTM_TERM_CF_ID = 282431;
-    public const UTM_CONTENT_CF_ID = 282423;
+    public const FBP_CF_ID = 976191;
+    public const GOOGLE_CID_CF_ID = 842127;
+    public const UTM_SOURCE_CF_ID = 842107;
+    public const UTM_MEDIUM_CF_ID = 842103;
+    public const UTM_CAMPAIGN_CF_ID = 842105;
+    public const UTM_TERM_CF_ID = 842109;
+    public const UTM_CONTENT_CF_ID = 842101;
     public const CUSTOM_SOURCE_CF_ID = '';
-    public const REFERRER_CF_ID = 282437;
+    public const REFERRER_CF_ID = 842111;
 
 
     /* GETCOURSE */
-    public const CF_GC_DEAL_ID = 379049;
-    public const CF_GC_DEAL_LINK = 378999;
-    public const CF_GC_PAYMENT_LINK = 379051;
-    public const CF_GC_PAID_MONEY = 379053;
-    public const CF_GC_REMAINING_TO_PAY = 379055;
-    public const CF_GC_POSITIONS = 379105;
+    public const CF_GC_DEAL_ID = 976181;
+    public const CF_GC_DEAL_LINK = 976179;
+    public const CF_GC_PAYMENT_LINK = 976183;
+    public const CF_GC_PAID_MONEY = 976193;
+    #public const CF_GC_REMAINING_TO_PAY = 379055;
+    public const CF_GC_POSITIONS = 976177;
 
 
     /* CONTACT CF */
-    public const PHONE_CF_ID = 282415;
-    public const EMAIL_CF_ID = 282417;
-    public const CF_GC_USER_LINK = 379251;
-    public const CF_GC_USER_ID = 543049;
+    public const PHONE_CF_ID = 842093;
+    public const EMAIL_CF_ID = 842095;
+    public const CF_GC_USER_LINK = 976187;
+    public const CF_GC_USER_ID = 976185;
 
     /* PIPELINES */
-    public const PIPELINES_ID_FOR_UA = [4963063,4972738];
-    public const PIPELINE_WORKED = 4963063;
-    public const PIPELINE_PAID = 4972738;
+    public const PIPELINES_ID_FOR_UA = [6069535];
+    public const PIPELINE_WORKED = 6069535;
+    public const PIPELINE_PAID = 6069535;
 
     /* STATUSES */
     /* Отдел доходимости */
-    public const STATUS_REGISTRATION = 44821654;
-    public const STATUS_SEND_WEBINAR_LINK = 44821657;
-    public const STATUS_START_WEBINAR = 44821660;
-    public const STATUS_VIEW_CONTENT_PART = 44821663;
-    public const STATUS_OPENED_RECORD_WEBINAR = 44890363;
+    public const STATUS_REGISTRATION = 52648171;
+    public const STATUS_OPEN_1_LESSON = 52650076;
+    public const STATUS_OPEN_2_LESSON = 52650079;
+    public const STATUS_OPEN_3_LESSON = 52650082;
+    public const STATUS_REGISTRATION_TO_WEBINAR = 52648174;
+    public const STATUS_OPENING_WEB = 52650085;
+
     public const STATUS_BOUNCE = 143;
 
 
 
     /* Отдел продаж */
-    public const STATUS_ALL_LEADS = 44890396;
-    public const STATUS_FORM_SUBMIT = 44890438;
-    public const STATUS_PARTIAL_PAYMENT_SUCCESS = 44890441;
+    public const STATUS_ALL_LEADS = 52650088;
+    public const STATUS_BUILD = 52650094;
+    public const STATUS_CREATED_ORDER = 52648177;
+    public const STATUS_PARTIAL_PAYMENT_SUCCESS = 52650097;
     public const STATUS_PAID_SUCCESS = 142;
 
 
@@ -68,8 +71,8 @@ class amocrmConfig
     public const PIPELINES_WORKED = [self::PIPELINE_WORKED,self::PIPELINE_PAID];
 
     /* USERS */
-    public const USER_ROP = 7740154;
-    public const RESPONSIBLE_USER_ID = 7740154;
+    public const USER_ROP = 7820154;
+    public const RESPONSIBLE_USER_ID = 7820154;
 
 
     /** SERVICE MESSAGE */
@@ -84,7 +87,7 @@ class amocrmConfig
     {
         return [
             'gc_deal_link' => ['id' => self::CF_GC_DEAL_LINK, 'type' => 'text'],
-            'left_cost_money' => ['id' => self::CF_GC_REMAINING_TO_PAY, 'type' => 'text'],
+            #'left_cost_money' => ['id' => self::CF_GC_REMAINING_TO_PAY, 'type' => 'text'],
             'deal_id' => ['id' => self::CF_GC_DEAL_ID, 'type' => 'text'],
             'positions' => ['id' => self::CF_GC_POSITIONS, 'type' => 'text'],
             'cost_money' => ['id' => self::CF_GC_PAID_MONEY, 'type' => 'text'],
