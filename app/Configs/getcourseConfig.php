9d58e5c583d6cb94d306bcbe54d557115c8d698c
EvgeniyLutsenko <evgnelucen@gmail.com>
2022-11-25 15:28:34 +0300

    add new domain

--- app/Configs/getcourseConfig.php
+++ app/Configs/getcourseConfig.php
@@ -6,9 +6,8 @@ namespace App\Configs;
 
 class getcourseConfig
 {
-    //TODO CHANGE ME
-    public const GC_SUBDOMAIN = 'easyplustest1';
-    public const ACCOUNT_LINK = 'https://klub.easyplus.online/';
+
+    public const ACCOUNT_LINK = 'https://school.ivanova-finance.ru/';
     public const DEAL_LINK = self::ACCOUNT_LINK."sales/control/deal/update/id/";
     public const USER_LINK = self::ACCOUNT_LINK."user/control/user/update/id/";
 
