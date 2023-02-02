2fe3ca8cdd2f68f26264bf6e5322f0ce6411cdc8
Evgeniy <lutcenko.evgeniy@adv.ru>
2023-02-02 23:48:20 +0300

    Добавлена логика работы с нулевым заказом

--- app/Services/AmoCRM/Lead/CreateOrUpdateLeadByDealDtoAndContact.php
+++ app/Services/AmoCRM/Lead/CreateOrUpdateLeadByDealDtoAndContact.php
@@ -9,7 +9,10 @@ use AmoCRM\Models\ContactModel;
 use AmoCRM\Models\LeadModel;
 use App\Models\DTO\Action\AmoActionDto;
 use App\Models\Dto\Getcourse\DealDto;
+use App\Services\AmoCRM\Pipelines\Statuses\GetPriorityStatusByAmoActionDto;
+use App\Services\AmoCRM\Pipelines\Statuses\GetPriorityStatusByLeadModel;
 
+/** Не используй это в других проектах. Тут заложена бизнес логика проекта Время Порядка. */
 class CreateOrUpdateLeadByDealDtoAndContact
 {
     /**
@@ -25,28 +28,51 @@ class CreateOrUpdateLeadByDealDtoAndContact
         AmoActionDto $action_params = null
     )
     {
-        # ищем сделки
+        # ищем открытые сделки полученного контакта
         $leads_collection = GetOpenedLeadsByContactModel::run($api_client, $contact);
 
         if (!$leads_collection->isEmpty()) {
-            # если нашли - обновляем
-            $lead = $leads_collection->first();
-            $lead = UpdateLeadModelByDealDto::run($lead,$deal);
-            if(!empty($action_params)) {  $lead = UpdateLeadModelByAmoActionDto::run($lead, $action_params); }
-            $lead = UpdateLeadByLeadModel::run($api_client, $lead);
+            # Если нашли
+            for($i = 0;$i < $leads_collection->count();$i++ ) {
+                $lead = $leads_collection->offsetGet($i);
+                # ищем среди них сделку с нулевым бюджетом
+                if (self::isZeroCostDeal($lead)) {
+                    $lead = $leads_collection->first();
+                    $lead = UpdateLeadModelByDealDto::run($lead,$deal);
+                    if(null !== $action_params) {
+                        $leadPriorityStatus = GetPriorityStatusByLeadModel::run($lead);
+                        $actionPriorityStatus = GetPriorityStatusByAmoActionDto::run($action_params);
+                        # если у найденного лида приоритет статуса больше чем у действия - обновляем действие.
+                        if($leadPriorityStatus > $actionPriorityStatus){
+                            $action_params->setStatusId($lead->getStatusId());
+                            $action_params->setPipelineId($lead->getPipelineId());
+                        }
+
+                        $lead = UpdateLeadModelByAmoActionDto::run($lead, $action_params);
+                    }
+                    # и обновляем её и выходим
+                    return UpdateLeadByLeadModel::run($api_client, $lead);
+                }
+            }
+
         }
-        //$lead = GetLeadWithoutDealId::run($leads_collection);
-        # если не нашли - создаем
-        else {
-            $lead = new LeadModel();
-            $lead = UpdateLeadModelByDealDto::run($lead, $deal);
-            if(!empty($action_params)) { $lead = UpdateLeadModelByAmoActionDto::run($lead, $action_params); }
-            $lead = CreateLeadByLeadModel::run($api_client, $lead);
-            # связываем с контактом
-            $linked = LinkedToContact::run($api_client,$lead,$contact);
+        # если не нашли нулевую сделку или вообще сделок у контакта
+        # создаем новую
+        $lead = new LeadModel();
+        $lead = UpdateLeadModelByDealDto::run($lead, $deal);
+        if(null !== $action_params) {
+            $lead = UpdateLeadModelByAmoActionDto::run($lead, $action_params);
         }
+        $lead = CreateLeadByLeadModel::run($api_client, $lead);
+        # связываем её с контактом.
+        $linked = LinkedToContact::run($api_client,$lead,$contact);
 
         return $lead;
+    }
+
+    private static function isZeroCostDeal(LeadModel $lead): bool
+    {
+        return 0 == $lead->getPrice() || empty($lead->getPrice());
     }
 
 }
---
+++ app/Services/AmoCRM/Pipelines/Statuses/GetPriorityStatusByAmoActionDto.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Services\AmoCRM\Pipelines\Statuses;
+
+
+use App\Models\AmoCRM\Status;
+use App\Models\Dto\Action\AmoActionDto;
+
+class GetPriorityStatusByAmoActionDto
+{
+    public static function run(AmoActionDto $actionDto)
+    {
+        $result = Status::select('id')
+            ->where('status_id','=',$actionDto->getStatusId())
+            ->where('pipeline_id', '=', $actionDto->getPipelineId())
+            ->get();
+
+        if(!empty($result->first())){
+            return $result->first()->id;
+        } else {
+            return null;
+        }
+    }
+
+}
