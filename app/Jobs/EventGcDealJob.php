319b6256390cd80a0608b457f96d442679701d1c
Evgeniy <lutcenko.evgeniy@adv.ru>
2023-02-15 11:45:23 +0300

    выключил serializer

--- app/Jobs/EventGcDealJob.php
+++ app/Jobs/EventGcDealJob.php
@@ -15,11 +15,10 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 
 class EventGcDealJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable;
 
 
     public EventGcDealJobCommand $command;
